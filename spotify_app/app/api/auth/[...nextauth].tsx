c00d695c3bd279c22b7cd5642fe5e602756856ee
Bommisrilekkhaa <bommisrilekkhaaganesan@gmail.com>
2023-11-03 21:28:14 +0530

    api_integration

--- spotify_app/app/api/auth/[...nextauth].tsx
+++ spotify_app/app/api/auth/[...nextauth].tsx
@@ -5,13 +5,28 @@ import SpotifyProvider from "next-auth/providers/spotify"
 export default NextAuth({
     providers: [
         SpotifyProvider({
-            clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
-            clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+            clientId: "cf8226e47f044dda9783369518711821",
+            clientSecret: "614a8e52d3ea45a0853b0333d40d515c",
             authorization: LOGIN_URL,
         })
     ],
     secret:process.env.JWT_SECRET,
     pages:{
         signIn:'/login'
+    },
+    callbacks: {
+        async jwt({ token, account, user }){
+            //initial signin
+            if(account && user){
+                return {
+                    ...token,
+                    accessToken: account.access_token,
+                    refreshToken: account.refresh_token,
+                    username: account.providerAccountId,
+                    accessTokenExpires: account.expires_at * 1000,
+                }
+            }
+            if(Date.now() < )
+        }
     }
 })
